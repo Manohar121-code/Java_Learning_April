48d27361bf5266d3a7c2442ce006f06da8e6abc8
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-04-09 09:48:37 +0530

    Initial commit

---
+++ src/com/learn/basics/DataTypes.java
@@ -0,0 +1,36 @@
+package com.learn.basics;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		System.out.println("Main method started");
+
+		char ch = '#';
+		byte b = 98;
+		short sh = 150;
+		int i = 100000;
+		long l = 68768768;
+
+		float f = 13.46f;
+		double d = 5622.142;
+
+		boolean check = true;
+
+		System.out.println(ch);
+		System.out.println(b);
+		System.out.println(sh);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(check);
+
+		int intValue; //declaration
+		intValue = 10; //initialization
+
+		System.out.println(intValue);
+		
+		int givenTheNameVeryBig = 10;
+
+		System.out.println("Main method ended");
+	}
+}
