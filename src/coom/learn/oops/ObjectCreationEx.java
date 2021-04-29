eed31d45e6f35191f8408435ecad74f350fba9cf
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-04-29 09:12:13 +0530

    OOPS

---
+++ src/coom/learn/oops/ObjectCreationEx.java
@@ -0,0 +1,38 @@
+package coom.learn.oops;
+
+public class ObjectCreationEx {
+	int id;
+	String name;
+	
+	public void m1() {
+		System.out.println("m1() called");
+	}
+
+	public static void main(String[] args) {
+//		<classType> <identifier> = new <classType>();
+		ObjectCreationEx obj1 = new ObjectCreationEx();
+		obj1.id = 10;
+		obj1.name = "java";
+		System.out.println(obj1.id);
+		System.out.println(obj1.name);
+		obj1.m1();
+		
+		System.out.println("---------------------------");
+		
+		ObjectCreationEx obj2 = new ObjectCreationEx();
+		obj2.id = 20;
+		obj2.name = "python";
+		System.out.println(obj2.id);
+		System.out.println(obj2.name);
+		obj2.m1();
+
+		System.out.println("---------------------------");
+		
+		ObjectCreationEx obj3 = new ObjectCreationEx();
+		obj3.id = 30;
+		obj3.name = "Go lang";
+		System.out.println(obj3.id);
+		System.out.println(obj3.name);
+		obj3.m1();
+	}
+}
